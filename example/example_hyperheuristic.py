bc153e6a68315e21594e959c96c37e3351e23e82
SealtielFreak <sealtiel@email.com>
2026-05-26 15:09:52 -0600

    OpenCL testing

---
+++ example/example_hyperheuristic.py
@@ -0,0 +1,18 @@
+def define_h(f):
+    return f
+
+
+@define_h
+def random_search_h():
+    def a():
+        pass
+
+    def b():
+        pass
+
+
+    def config():
+        pass
+
+
+    return config
