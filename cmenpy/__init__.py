706d800424e281c9f7cd56b2e57a2194f1148535
Sealtiel <sealtiel@email.com>
2026-05-31 13:21:05 -0600

    Mo variables

--- cmenpy/__init__.py
+++ cmenpy/__init__.py
@@ -1,6 +1,6 @@
 __version__ = "2026"
 
-from cmenpy.argument import Argument, Variable
+from cmenpy.model.optimizer.variables import Argument, Variable
 from cmenpy.bounds import Bounds
 from cmenpy.model import declare, declare_check
 from cmenpy.model.optimizer import FunctionOptimizerModel, TemplateOptimizerModel
