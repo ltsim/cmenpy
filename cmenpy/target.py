15d760a06b6f22315bb9a137c75b73a5a1d9f01b
Sealtiel <sealtiel@email.com>
2026-06-01 17:17:50 -0600

    Reformat class and apply along axis mode, for Agent buffer

--- cmenpy/target.py
+++ cmenpy/target.py
@@ -1,36 +1,43 @@
 import typing
 
 import numpy as np
-import numpy.typing as npt
 
 from cmenpy.bounds import Bounds
-
-ScalarType = typing.TypeVar("ScalarType", bound=np.number)
+from cmenpy.types import NDArrayType, ArrayIntegerType
 
 
 class MaxNumberOfCalls(Exception): ...
 
 
 class Target(typing.Protocol):
-    def __call__(self, x: npt.NDArray[ScalarType]) -> ScalarType | typing.Any: ...
+    def __call__(self, x: NDArrayType) -> NDArrayType | typing.Any: ...
 
 
 class TargetFunction:
     def __init__(
         self, target: Target, bounds: Bounds, max_nfe: typing.Optional[int] = None
     ) -> None:
-        self.__target = target
+        def wrapper(x: NDArrayType) -> NDArrayType | typing.Any:
+            if not self.__max_nfe > self.__nfe:
+                raise MaxNumberOfCalls("Max number of function calls exceeded.")
+
+            self.__nfe += 1
+
+            return target(x)
+
+        self.__target = wrapper
         self.__bounds = bounds
         self.__nfe = 0
         self.__max_nfe = np.inf if max_nfe is None else max_nfe
 
-    def __call__(self, x: npt.NDArray[ScalarType]) -> ScalarType | typing.Any:
-        if not self.__max_nfe > self.__nfe:
-            raise MaxNumberOfCalls("Max number of function calls exceeded.")
-
-        self.__nfe += 1
-
+    def __call__(self, x: NDArrayType) -> NDArrayType | typing.Any:
         return self.__target(x)
+
+    def evaluate(self, x: NDArrayType) -> NDArrayType | typing.Any:
+        return self.__target(x)
+
+    def apply_axis(self, x: NDArrayType, mask: ArrayIntegerType) -> NDArrayType:
+        return np.apply_along_axis(self.__target, 1, x[mask, 1:])
 
     @property
     def nfe(self) -> int:
@@ -46,4 +53,4 @@ class TargetFunction:
 
     @property
     def f(self) -> Target:
-        return lambda x: self.__call__(x)
+        return self.evaluate
