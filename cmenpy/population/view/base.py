180544d83826d8ce4f557c45e55d5b6346cae161
Sealtiel <sealtiel@email.com>
2026-06-01 17:43:49 -0600

    Abstract class for work with Population list objects

--- cmenpy/population/view/base.py
+++ cmenpy/population/view/base.py
@@ -1,2 +1,40 @@
-class PopulationA:
-    pass
+import abc
+
+from cmenpy.agent import Agent
+from cmenpy.types import NDArrayType
+
+
+class BasePopulation(abc.ABC):
+    @abc.abstractmethod
+    def __iter__(self): ...
+
+    @abc.abstractmethod
+    def __repr__(self): ...
+
+    @abc.abstractmethod
+    def __getitem__(self, key): ...
+
+    @abc.abstractmethod
+    def __setitem__(self, key, value): ...
+
+    @abc.abstractmethod
+    def __matmul__(self, other): ...
+
+    @abc.abstractmethod
+    def __imatmul__(self, other): ...
+
+    @property
+    @abc.abstractmethod
+    def size(self) -> int: ...
+
+    @property
+    @abc.abstractmethod
+    def all(self) -> list[Agent]: ...
+
+    @property
+    @abc.abstractmethod
+    def fitnesses(self) -> NDArrayType: ...
+
+    @property
+    @abc.abstractmethod
+    def solutions(self) -> NDArrayType: ...
