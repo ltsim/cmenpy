e7e2e6f7731e444535880d4b31405170227192ec
Sealtiel <sealtiel@email.com>
2026-06-01 01:24:35 -0600

    Fix importation

--- cmenpy/tracker/__init__.py
+++ cmenpy/tracker/__init__.py
@@ -1,12 +1,17 @@
+import collections
 import typing
 
+from cmenpy.population import Population
 from cmenpy.tracker.epoch import EpochHistory
 
 
 class Tracker:
-    def __init__(self, history: typing.Collection[EpochHistory]):
-        self.___history = history
+    def __init__(self, population: Population):
+        self.___history = collections.deque()
+        self.___population = population
 
     @property
     def history(self) -> list[EpochHistory]:
         return [*self.___history]
+
+    def track(self) -> None: ...
