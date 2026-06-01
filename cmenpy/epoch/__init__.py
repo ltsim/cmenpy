4eaa6081003e3a5cd899dbbb2d92c2607ad2e496
Sealtiel <sealtiel@email.com>
2026-05-31 22:24:11 -0600

    Timeit in iterations

--- cmenpy/epoch/__init__.py
+++ cmenpy/epoch/__init__.py
@@ -1,3 +1,6 @@
+import time
+
+
 class EpochIteration:
     def __init__(
         self,
@@ -8,11 +11,15 @@ class EpochIteration:
 
         self.__epochs: int = epochs
         self.__current_epoch: int = 0
+        self.__cpu_time: float = 0
 
     def __iter__(self):
         for i in range(0, self.__epochs):
             self.__current_epoch = i
+
+            start = time.process_time()
             yield i
+            self.__cpu_time = time.process_time() - start
 
     @property
     def current(self):
@@ -21,3 +28,7 @@ class EpochIteration:
     @property
     def max(self) -> int:
         return self.__epochs
+
+    @property
+    def cpu_time(self):
+        return self.__cpu_time
