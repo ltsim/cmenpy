044db684b115f556a74667eef3cfce2fb0807010
SealtielFreak <sealtiel@email.com>
2026-06-04 16:12:49 -0600

    Fix GWO (A & C constant)

--- example/gwa_algorithm.py
+++ example/gwa_algorithm.py
@@ -9,27 +9,26 @@ def gwo(args, epoch, ctx):
     pop, bounds, rng = ctx.population, ctx.bounds, ctx.rng
 
     pop.compute << rng.uniform(bounds.low, bounds.up, (len(pop), bounds.ndim))
+    X_n = np.full((pop.size, bounds.ndim), np.nan)
 
     for e in epoch:
-        all_pop = cm.sort_agents(pop)
+        all_pop = cm.sort_agents(pop, ctx.sense)
         new_pop = pop.view
 
         best_pop = all_pop[:3]
 
         a = 2 - 2 * e / epoch.max
 
-        A = a * (2 * rng.uniform(size=(pop.size, bounds.ndim)) - 1)
-        C = 2 * rng.uniform(size=(pop.size, bounds.ndim))
-
-        X_n = np.zeros((pop.size, bounds.ndim))
-
         for i, p in enumerate(all_pop):
-            xn = [
-                b.solution - A[i] * np.abs(C[i] * b.solution - p.solution)
-                for b in best_pop
+            A = a * (2 * rng.random(size=(len(best_pop), bounds.ndim)) - 1)
+            C = 2 * rng.random(size=(len(best_pop), bounds.ndim))
+
+            D = [
+                b.solution - A[j] * np.abs(C[j] * b.solution - p.solution)
+                for j, b in enumerate(best_pop)
             ]
 
-            X_n[i] = np.sum(xn, axis=0) / 3
+            X_n[i] = np.sum(D, axis=0) / len(best_pop)
 
         new_pop.compute << np.clip(
             X_n,
@@ -38,15 +37,12 @@ def gwo(args, epoch, ctx):
         )
 
         for i, (a, b) in enumerate(zip(new_pop, all_pop)):
-            if b := cm.best_of((a, b)):
-                pop.assign[i] << b.buff
+            pop.assign[i] << cm.best_of((a, b), ctx.sense).buff
 
 
 if __name__ == "__main__":
     model = gwo()
     f = Ackley01(ndim=30)
 
-    b_pop = model.solve(
-        f.evaluate, [[-5.12, 5.12] for _ in range(f.ndim)], 130, 75, debug=True
-    )
+    b_pop = model.solve(f.evaluate, f.bounds, 150, 75)
     print(b_pop)
