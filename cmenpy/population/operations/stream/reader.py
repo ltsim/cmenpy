b84120949a13e13aaa0e3f4123b7624c00324bbe
SealtielFreak <sealtiel@email.com>
2026-06-04 20:53:09 -0600

    Implemented operator

--- cmenpy/population/operations/stream/reader.py
+++ cmenpy/population/operations/stream/reader.py
@@ -7,3 +7,9 @@ from cmenpy.types import NDArrayType
 class ReaderOperator(StreamOperator):
     @abc.abstractmethod
     def __getitem__(self, item: int | slice) -> NDArrayType: ...
+
+    @abc.abstractmethod
+    def __invert__(self) -> NDArrayType: ...
+
+    @abc.abstractmethod
+    def __rshift__(self, other: NDArrayType) -> NDArrayType: ...
