8728b13b477a953b70e0d923bc2cd992e74b49b7
SealtielFreak <sealtiel@email.com>
2026-06-04 20:54:30 -0600

    Minor changes

--- cmenpy/population/base.py
+++ cmenpy/population/base.py
@@ -1,8 +1,8 @@
 import abc
 
-from cmenpy.population.operations.extract import ExtractOperation
 from cmenpy.population.operations.assign import AssignOperator
 from cmenpy.population.operations.compute import ComputeOperator
+from cmenpy.population.operations.extract import ExtractOperation
 from cmenpy.population.operations.swap import SwapOperation
 
 
