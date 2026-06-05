a97079612a9d401ec83486206f0e3a8c7941f295
SealtielFreak <sealtiel@email.com>
2026-06-04 20:10:12 -0600

    Implemented population generator

---
+++ cmenpy/population/base.py
@@ -0,0 +1,19 @@
+import abc
+
+from cmenpy.population.operations.assign import AssignOperator
+from cmenpy.population.operations.compute import ComputeOperator
+from cmenpy.population.operations.swap import SwapOperation
+
+
+class PopulationOperationBase(abc.ABC):
+    @property
+    @abc.abstractmethod
+    def swap(self) -> SwapOperation: ...
+
+    @property
+    @abc.abstractmethod
+    def compute(self) -> ComputeOperator: ...
+
+    @property
+    @abc.abstractmethod
+    def assign(self) -> AssignOperator: ...
