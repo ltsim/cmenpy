eade4d3777187546e5a414d1ba64b2cd36dd537c
Sealtiel <sealtiel@email.com>
2026-06-08 01:50:15 -0600

    Reimplement functools and typed

--- cmenpy/functools/__init__.py
+++ cmenpy/functools/__init__.py
@@ -4,33 +4,37 @@ from cmenpy.functools.utils import sort_agents
 from cmenpy.population.agent import Agent
 from cmenpy.types.option import SenseType
 
+A = typing.TypeVar("A")
+B = typing.TypeVar("B")
+AB = tuple[A, B]
 
-def is_best(ab: typing.Any, sense: SenseType = "min") -> bool:
+
+def is_best(ab: AB, sense: SenseType = "min") -> bool:
     a, b = ab
 
     if sense == "min":
-        return a[0] < b[0]
+        return a < b
 
-    return a[0] > b[0]
+    return a > b
 
 
-def is_worst(ab: typing.Any, sense: SenseType = "min") -> bool:
+def is_worst(ab: AB, sense: SenseType = "min") -> bool:
     a, b = ab
 
     if sense == "min":
-        return a[0] > b[0]
+        return a > b
 
-    return a[0] < b[0]
+    return a < b
 
 
-def best_of(agents: typing.Collection[Agent], sense: SenseType = "min") -> Agent:
+def best_of(agents: typing.Collection[A], sense: SenseType = "min") -> Agent:
     if sense == "min":
         return min(agents)
 
     return max(agents)
 
 
-def worst_of(agents: typing.Collection[Agent], sense: SenseType = "min") -> Agent:
+def worst_of(agents: typing.Collection[A], sense: SenseType = "min") -> Agent:
     if sense == "max":
         return min(agents)
 
