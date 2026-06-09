f9a41e37c42405fbbdf0599391ba5e2d513f1ae5
Sealtiel <sealtiel@email.com>
2026-06-09 09:51:05 -0600

    Implemented Generator agent class

---
+++ cmenpy/population/agent/generator.py
@@ -0,0 +1,7 @@
+from cmenpy.kernel import KernelBuffer
+from cmenpy.hints.option import SenseType
+
+
+class AgentGenerator:
+    def __init__(self, buffer: KernelBuffer, sense: SenseType = "min"):
+        pass
