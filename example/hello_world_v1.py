04cde873c5bceb66f8088da3e084310e0556499d
SealtielFreak <sealtiel@email.com>
2026-06-09 15:43:10 -0600

    Minor changes

--- example/hello_world_v1.py
+++ example/hello_world_v1.py
@@ -7,7 +7,7 @@ import cmenpy as cm
 def my_algorithm(args, epoch, ctx):
     buff, bounds, rng = ctx.buff, ctx.bounds, ctx.rng
 
-    pop = cm.PopulationSwarm(buff, ctx.sense)
+    pop = cm.PopulationSwarm(buff)
 
     pop.compute << rng.uniform(bounds.low, bounds.up, (pop.size, bounds.ndim))
     b_pop = pop.F[pop.best]
