2c2bb4ff7dc148171cc6b96a5822170f33361641
SealtielFreak <sealtiel@email.com>
2026-06-09 15:43:05 -0600

    Refactoring imports from module

--- cmenpy/population/operations/__init__.py
+++ cmenpy/population/operations/__init__.py
@@ -1,6 +1,13 @@
+from cmenpy.population.operations.access import AccessOperator
 from cmenpy.population.operations.assign import AssignOperator
 from cmenpy.population.operations.compute import ComputeOperator
 from cmenpy.population.operations.extract import ExtractOperation
 from cmenpy.population.operations.swap import SwapOperation
 
-__all__ = ["AssignOperator", "ComputeOperator", "ExtractOperation", SwapOperation]
+__all__ = [
+    "AssignOperator",
+    "ComputeOperator",
+    "ExtractOperation",
+    "SwapOperation",
+    "AccessOperator",
+]
