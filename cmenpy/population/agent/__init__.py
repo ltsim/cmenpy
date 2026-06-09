b68bb38b926a3ece182717952151e222c5171ba6
Sealtiel <sealtiel@email.com>
2026-06-09 09:51:25 -0600

    Refactoring module agent

---
+++ cmenpy/population/agent/__init__.py
@@ -0,0 +1,6 @@
+from cmenpy.population.agent.base import BaseAgent
+from cmenpy.population.agent.inmutable import ImmutableAgent
+from cmenpy.population.agent.mutable import MutableAgent
+from cmenpy.population.agent.generator import AgentGenerator
+
+__all__ = ["AgentGenerator", "BaseAgent", "MutableAgent", "ImmutableAgent"]
