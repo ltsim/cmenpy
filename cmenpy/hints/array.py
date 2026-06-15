6b45fb0b41a811708f80e5ba73a66bb477c496e6
El Pinche Sealtiel <43727474+SealtielFreak@users.noreply.github.com>
2026-06-15 16:24:33 -0600

    Implemented new types for Arrays

--- cmenpy/hints/array.py
+++ cmenpy/hints/array.py
@@ -6,3 +6,5 @@ import numpy.typing as npt
 NDArrayType = npt.NDArray[np.number[typing.Any]]
 ArrayFloatType = npt.NDArray[np.floating[typing.Any]]
 ArrayIntegerType = npt.NDArray[np.integer[typing.Any]]
+ArrayBoolType = npt.NDArray[np.bool_]
+ArrayObjectType = npt.NDArray[np.object_]
